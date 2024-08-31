8cec0aeaa620e254157f2598901fb142bcfe43be
SUBIN S <subinvidhu3232@gmail.com>
2024-08-31 14:16:29 +0530

    Update portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -6,9 +6,11 @@ import os
 class Portfolio:
     def __init__(self):
         current_dir = os.path.dirname(__file__)
+        parent_dir = os.path.dirname(current_dir)
         self.file_path = os.path.join(current_dir, "resource", "my_portfolio.csv")
         self.data = pd.read_csv(self.file_path)
-        self.chroma_client = chromadb.PersistentClient('vectorstore')
+        vectorstore_path = os.path.join(parent_dir, "vectorstore")
+        self.chroma_client = chromadb.PersistentClient(vectorstore_path)
         self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
